d393e18a7f4e45209fd04712ce7eb45ac7963490
HenriqueRiciole <hriciole@gmail.com>
2023-05-02 20:43:18 -0300

    Desafio Placar atualizado

--- src/components/score/Score.jsx
+++ src/components/score/Score.jsx
@@ -2,18 +2,19 @@ import styles from './Score.module.css'
 
 import Icon from '../icon/Icon'
 
-function Score(){
+function Score({xWin, circleWin}){
+
     return(
         <>
           <h4>Placar :</h4>
           <div className={styles.score}>
             <div className={styles.scoreContent}>
                 <Icon iconName="circle"/>
-                <h2>0</h2>
+                <h2>{circleWin}</h2>  
             </div>
             <div className={styles.scoreContent}>
                 <Icon iconName="x"/>
-                <h2>0</h2>
+                <h2>{xWin}</h2>
             </div>
           </div>
         </>
